fadaed9ba63a71f36f76428ee59302e811e19a29
Arash <arashsaleh798@gmail.com>
2025-03-12 15:44:44 -0700

    setup complete and all tracks posted

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,1 +1,17 @@
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+const index = async () => {
+  try {
+    //GET (grabbing tracks from back end)
+    const res = await fetch(BASE_URL);
+    return res.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+
+
+export {
+  index,
+};
